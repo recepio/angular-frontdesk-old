a8645b0a8441e02f5526478fd7891aaf1ab7c619
hankypanky <menshakov.artem@gamil.com>
2016-09-26 21:42:18 +0300

    create slider for rooms & timeline

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -13,12 +13,12 @@ export class TimelineComponent implements OnInit {
   rooms: number[] = [];
   widthColumn: number;
   qtyDays: number = 15;
+  timelineMarginRight: number = 0;
 
   constructor() {
     this.today = new Date();
     this.createDays();
     this.createRooms();
-
   }
 
   ngOnInit(): void {
@@ -41,7 +41,15 @@ export class TimelineComponent implements OnInit {
   }
 
   nextClick(): void {
-    console.log('click');
+    let lastDay: Date = new Date(this.days[this.days.length - 1]);
+    this.days.push(lastDay.setDate(lastDay.getDate() + 1));
+    this.timelineMarginRight += -this.widthColumn;
+  }
+
+  prevClick(): void {
+    if(this.timelineMarginRight !== 0) {
+      this.timelineMarginRight += this.widthColumn;
+    }
   }
 
 }
