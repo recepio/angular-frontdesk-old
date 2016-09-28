40dfbefe5fbb2be03b0df32eb5182eb5f5d29e06
hankypanky <menshakov.artem@gamil.com>
2016-09-28 19:15:52 +0300

    create full timeline component without styles

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 declare var moment: any;
 
 @Component({
@@ -8,49 +8,80 @@ declare var moment: any;
 })
 
 export class TimelineComponent implements OnInit {
-  today: Date;
-  days: number[] = [];
-  rooms: number[] = [];
-  widthColumn: number;
-  qtyDays: number = 15;
-  timelineMarginRight: number = 0;
+  today: Date; //
+  days: number[] = []; // arr for days
+  rooms: number[] = []; // arr for rooms
+  widthColumn: number; //
+  widthDay: number = 15; // width day col
+  timelineMarginRight: number = 0; // save margin
+  nextButtonClick: number = 0; // counter clicks
+  disableButton: string = 'disabled';
+
+  indexOfLastArrElement: number = 0;
+  nextMonth: Date;
+  prevMonth: Date;
 
   constructor() {
     this.today = new Date();
     this.createDays();
     this.createRooms();
+    this.nextMonth = new Date(this.today.getFullYear(), this.today.getMonth() + 1);
+    this.prevMonth = new Date(this.today);
   }
 
   ngOnInit(): void {
     let timeLineWidth: number = document.querySelector('.timeline').clientWidth;
-    this.widthColumn = timeLineWidth / this.qtyDays;
+    this.widthColumn = timeLineWidth / this.widthDay;
+    this.timelineMarginRight = this.widthColumn;
+    this.widthColumn = +this.widthColumn.toFixed();
   }
 
+  // create mocks days
   createDays(): void {
     let today: Date = new Date();
     today.setDate(today.getDate() - 5);
-    for(let i = 1; i < this.qtyDays; i++) {
+    for (let i = 1; i < this.widthDay; i++) {
       this.days.push(today.setDate(today.getDate() + 1));
     }
   }
 
+  // create mocks rooms
   createRooms(): void {
-    for(let i = 1; i < 170; i++) {
+    for (let i = 1; i < 170; i++) {
       this.rooms.push(i);
     }
   }
 
-  nextClick(): void {
-    let lastDay: Date = new Date(this.days[this.days.length - 1]);
-    this.days.push(lastDay.setDate(lastDay.getDate() + 1));
-    this.timelineMarginRight += -this.widthColumn;
-  }
 
-  prevClick(): void {
-    if(this.timelineMarginRight !== 0) {
-      this.timelineMarginRight += this.widthColumn;
-      console.log();
+  timelineButtonClick(typeOfClick: string): void {
+    let lastDay: Date = new Date(this.days[this.days.length - 1]);
+
+    if (typeOfClick === 'nextClick') {
+
+      this.days.push(lastDay.setDate(lastDay.getDate() + 1));
+
+      this.timelineMarginRight += -this.widthColumn;
+      this.nextButtonClick++;
+      this.disableButton = '';
     }
+
+    if (typeOfClick === 'prevClick') {
+      if (this.nextButtonClick !== 0) {
+        this.timelineMarginRight += this.widthColumn;
+        this.nextButtonClick--;
+        this.indexOfLastArrElement++;
+      } else {
+        this.disableButton = 'disabled';
+        this.timelineMarginRight = this.widthColumn;
+      }
+    }
+
+    let lastDayonView = new Date(this.days[this.days.length - (1 + this.indexOfLastArrElement)]);
+
+    this.prevMonth = new Date(lastDayonView.getFullYear(), lastDayonView.getMonth() - 1);
+    this.nextMonth = new Date(lastDayonView.getFullYear(), lastDayonView.getMonth());
+    //console.log(lastDayonView);
+
   }
 
 }
